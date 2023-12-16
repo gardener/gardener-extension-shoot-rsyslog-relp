b99c4f254596e818ec98e59cd20ea761e3fc8add
Plamen Kokanov <plamen.kokanov@sap.com>
2023-12-16 13:17:52 +0200

    Embed the `rsyslog-relp-echo-server` chart
    The chart is embedded so that it can be reused to deploy the
    `rsyslog-relp-echo-server` in the testmachinery tests.

---
+++ example/local/charts.go
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package local
+
+import (
+	"embed"
+	"path/filepath"
+)
+
+var (
+	// Charts embeds the local charts in embed.FS.
+	//go:embed charts
+	Charts embed.FS
+
+	// ChartPath is the path to the example local charts.
+	ChartsPath = filepath.Join("charts")
+	// RsyslogRelpEchoServerChartPath is the path to the rsyslog-relp-echo-server local chart.
+	RsyslogRelpEchoServerChartPath = filepath.Join(ChartsPath, "rsyslog-relp-echo-server")
+)
