a49fa6cf2e8e4701d5ed4e83e8bafe463269b71a
ialidzhikov <i.alidjikov@gmail.com>
2024-02-12 15:33:15 +0200

    Clean up sed usage after ControllerRegistration generation

--- charts/gardener-extension-shoot-rsyslog-relp/doc.go
+++ charts/gardener-extension-shoot-rsyslog-relp/doc.go
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //go:generate sh -c "../../vendor/github.com/gardener/gardener/hack/generate-controller-registration.sh shoot-rsyslog-relp . $(cat ../../VERSION) ../../example/controller-registration.yaml Extension:shoot-rsyslog-relp"
-//go:generate sh -c "sed -i 's/ type: shoot-rsyslog-relp/ type: shoot-rsyslog-relp\\n    lifecycle:\\n      reconcile: AfterKubeAPIServer\\n      delete: BeforeKubeAPIServer\\n      migrate: BeforeKubeAPIServer/' ../../example/controller-registration.yaml"
 
 // Package chart enables go:generate support for generating the correct controller registration.
 package chart
