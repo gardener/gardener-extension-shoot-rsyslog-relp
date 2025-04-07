57de9dd2e9477b39681c4d65d3acf75676edbfd0
RadaBDimitrova <rada09@abv.bg>
2025-04-07 13:06:57 +0300

    implement base cases for webhook integration test

--- test/integration/webhook/webhook_test.go
+++ test/integration/webhook/webhook_test.go
@@ -5,8 +5,10 @@
 package webhook_test
 
 import (
+	"bytes"
 	_ "embed"
 	"encoding/json"
+	"text/template"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -15,16 +17,21 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/gardener-extension-shoot-rsyslog-relp/pkg/apis/rsyslog"
+	testcommon "github.com/gardener/gardener-extension-shoot-rsyslog-relp/test/common"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 )
 
-// //go:embed testdata/*
-// var testdata embed.FS
+type configData struct {
+	ProjectName string
+	Target      string
+	Port        int
+}
 
 var _ = Describe("Webhook tests", func() {
 	var (
-		osc    *extensionsv1alpha1.OperatingSystemConfig
-		config *rsyslog.RsyslogRelpConfig
+		osc        *extensionsv1alpha1.OperatingSystemConfig
+		config     *rsyslog.RsyslogRelpConfig
+		configData configData
 	)
 
 	JustBeforeEach(func() {
@@ -96,6 +103,7 @@ var _ = Describe("Webhook tests", func() {
 				},
 			},
 		}
+		configData.ProjectName = testNamespace.Name
 		By("Create OSC")
 		// we use Eventually because webhook may still not be ready to receive traffic
 		Eventually(func() error {
@@ -108,13 +116,72 @@ var _ = Describe("Webhook tests", func() {
 
 	})
 
-	Context("TODO", func() {
+	Context("Default Behaviour of extension with a default configuration", func() {
 		BeforeEach(func() {
-			config = &rsyslog.RsyslogRelpConfig{}
+			config = &rsyslog.RsyslogRelpConfig{
+				Target: "localhost",
+				Port:   1520,
+			}
+			configData.Target = "localhost"
+			configData.Port = 1520
 		})
 
-		It("TODO", func() {
-			//	_, _ = testdata.ReadFile("path/to/foo")
+		It("Should have all expected files attached", func() {
+			expectedFiles := []extensionsv1alpha1.File{
+				{
+					Path: "foo/bar",
+					Content: extensionsv1alpha1.FileContent{
+						Inline: &extensionsv1alpha1.FileContentInline{
+							Encoding: "b64",
+							Data:     "some-data",
+						},
+					},
+				},
+				{
+					Path: "/foo-bar-file",
+					Content: extensionsv1alpha1.FileContent{
+						ImageRef: &extensionsv1alpha1.FileContentImageRef{
+							Image:           "foo-image:bar-tag",
+							FilePathInImage: "/foo-bar-file",
+						},
+					},
+				},
+			}
+			// We use a tpl file to add the dynamically created in the test env .projectName
+			tmpl, err := template.New("config").Parse(string(testcommon.GetSimpleRsyslogConfig()))
+			Expect(err).NotTo(HaveOccurred())
+			var simpleRsyslogConfig bytes.Buffer
+			err = tmpl.Execute(&simpleRsyslogConfig, configData)
+			Expect(err).NotTo(HaveOccurred())
+
+			expectedFiles = append(expectedFiles, testcommon.GetRsyslogFiles(simpleRsyslogConfig.Bytes(), true)...)
+			expectedFiles = append(expectedFiles, testcommon.GetAuditRulesFiles(true)...)
+			Expect(len(expectedFiles)).To(Equal(len(osc.Spec.Files)))
+			for i, file := range osc.Spec.Files {
+				Expect(file).To(Equal(expectedFiles[i]))
+			}
+			Expect(expectedFiles).To(Equal(osc.Spec.Files))
+		})
+
+		It("Should have all expected units attached", func() {
+			expectedUnits := []extensionsv1alpha1.Unit{
+				{
+					Name: "foo",
+					DropIns: []extensionsv1alpha1.DropIn{
+						{
+							Name:    "drop1",
+							Content: "data1",
+						},
+					},
+					FilePaths: []string{"/foo-bar-file"},
+				},
+			}
+			expectedUnits = append(expectedUnits, []extensionsv1alpha1.Unit{testcommon.GetRsyslogConfiguratorUnit(true)}...)
+			Expect(len(expectedUnits)).To(Equal(len(osc.Spec.Units)))
+			for i, unit := range osc.Spec.Units {
+				Expect(unit).To(Equal(expectedUnits[i]))
+			}
+			Expect(expectedUnits).To(Equal(osc.Spec.Units))
 		})
 	})
 
