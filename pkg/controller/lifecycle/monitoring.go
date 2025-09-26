112527f593fac9ce288cbb5436361f0ca26f9a87
Luca Bernstein <luca.bernstein@sap.com>
2025-09-26 09:11:50 +0200

    Fix roles in `ScrapeConfig`s
    Since https://github.com/prometheus-operator/prometheus-operator/releases/tag/pkg/apis/monitoring/v0.76.0 only camel case roles are valid (see prometheus-operator/prometheus-operator@38900ce#diff-95caef4dacf48c47bf56afc00c513822feba29a5d2f6354b75c97a25a353d52fL75-R77)

--- pkg/controller/lifecycle/monitoring.go
+++ pkg/controller/lifecycle/monitoring.go
@@ -122,7 +122,7 @@ func deployMonitoringConfig(ctx context.Context, c client.Client, namespace stri
 			}},
 			KubernetesSDConfigs: []monitoringv1alpha1.KubernetesSDConfig{{
 				APIServer:  ptr.To("https://kube-apiserver"),
-				Role:       "endpoints",
+				Role:       "Endpoints",
 				Namespaces: &monitoringv1alpha1.NamespaceDiscovery{Names: []string{metav1.NamespaceSystem}},
 				Authorization: &monitoringv1.SafeAuthorization{Credentials: &corev1.SecretKeySelector{
 					LocalObjectReference: corev1.LocalObjectReference{Name: "shoot-access-prometheus-shoot"},
