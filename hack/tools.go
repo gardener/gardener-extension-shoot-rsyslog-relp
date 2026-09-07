530e6f852ce22535ced9da33e167bbf5a526c95b
Indrit Fejza <indrit.fejza@sap.com>
2026-09-07 15:59:20 +0200

    update gg to v1.150.2 and go to 1.26.5
    Signed-off-by: Indrit Fejza <indrit.fejza@sap.com>

--- hack/tools.go
+++ hack/tools.go
@@ -9,11 +9,7 @@
 package tools
 
 import (
-	_ "github.com/gardener/gardener/.github"
-	_ "github.com/gardener/gardener/.github/ISSUE_TEMPLATE"
-	_ "github.com/gardener/gardener/hack"
-	_ "github.com/gardener/gardener/hack/tools/logcheck/plugin"
-	_ "github.com/gardener/gardener/hack/tools/report-collector"
+	_ "github.com/gardener/gardener/hack/tools"
 
 	_ "github.com/elastic/crd-ref-docs"
 	_ "golang.org/x/tools/cmd/goimports"
